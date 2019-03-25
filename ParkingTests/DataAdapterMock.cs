2db951934ec3ea045cab466557ad2d2309da4b26
Egor Okhotin <egorokhotin@gmail.com>
2019-03-25 15:34:58 +0400

    remove warnings from dataAdapter mock

--- ParkingTests/DataAdapterMock.cs
+++ ParkingTests/DataAdapterMock.cs
@@ -2,12 +2,13 @@ using System.Threading.Tasks;
 using Parking.Data;
 using Parking.Models;
 using System;
+//using System.Threading.Tasks;
 
 namespace ParkingTests
 {
     public class DataAdapterMock : IDataAdapter
     {
-        DateTimeBuilder TimeBuilder {get;set;}
+        DateTimeBuilder TimeBuilder { get; set; }
 
         public DataAdapterMock()
         {
@@ -20,38 +21,42 @@ namespace ParkingTests
 
         public async Task<Parking.Models.Key> CreateKey(string tariffName)
         {
-            if(tariffName == null) throw new ArgumentException("Tariff name is null");
+            if (tariffName == null) throw new ArgumentException("Tariff name is null");
             return await CreateTestKey();
         }
 
         public async Task<Parking.Models.Key> CreateKey(string tariffName, string autoId)
         {
-            if(tariffName == null) throw new ArgumentException("Tariff name is null");
+            if (tariffName == null) throw new ArgumentException("Tariff name is null");
             return await CreateTestKey();
         }
 
         public async Task<Parking.Models.Key> FindKeyByAutoId(string autoId)
         {
-            if(autoId == null) throw new ArgumentNullException();
+            if (autoId == null) throw new ArgumentNullException();
             return await CreateTestKey();
         }
 
         public async Task<bool> DeleteKey(Parking.Models.Key key)
         {
-            if(key == null || key.Token==null)
-                return false;
-            return true;
+            return await Task.Run(() =>
+            {
+                if (key == null || key.Token == null)
+                    return false;
+                return true;
+            }
+            );
         }
 
         public async Task<Parking.Models.Key> FindKey(string token)
         {
-            if(token == null) throw new ArgumentException();
+            if (token == null) throw new ArgumentException();
             return await CreateTestKey();
         }
 
         public async Task<Parking.Models.Tariff> GetTariff(string tariffName)
         {
-            if(tariffName == null) throw new ArgumentException();
+            if (tariffName == null) throw new ArgumentException();
             return await CreateTestTariff();
         }
 
@@ -59,17 +64,20 @@ namespace ParkingTests
         {
             var k = new Parking.Models.Key();
             k.Tariff = await CreateTestTariff();
-            k.TimeStamp = TimeBuilder.GetTimeBeforeMinutes(10*2);
+            k.TimeStamp = TimeBuilder.GetTimeBeforeMinutes(10 * 2);
             k.Token = "TEST_TOKEN";
             return k;
         }
 
         private async Task<Parking.Models.Tariff> CreateTestTariff()
         {
-            var t = new Parking.Models.Tariff();
-            t.Cost = 10;
-            t.Name = "TEST_NAME";
-            return t;
+            return await Task.Run(() =>
+            {
+                var t = new Parking.Models.Tariff();
+                t.Cost = 10;
+                t.Name = "TEST_NAME";
+                return t;
+            });
         }
     }
 }
